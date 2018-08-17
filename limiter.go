d78db917de0c0f0439b5c6f27e03cb89f59a33b5
Aofei Sheng <aofei@aofeisheng.com>
2018-08-17 13:54:06 +0800

    feat: add limiter.go

---
+++ limiter.go
@@ -0,0 +1,30 @@
+package limiter
+
+import "github.com/sheng/air"
+
+// BodySizeGasConfig is a set of configurations for the `BodySizeGas()`.
+type BodySizeGasConfig struct {
+	MaxBytes                 int64
+	ErrRequestEntityTooLarge *air.Error
+}
+
+// BodySizeGas returns a gas that is used to limit ervery request's body size
+// based on the bsgc.
+func BodySizeGas(bsgc BodySizeGasConfig) air.Gas {
+	if bsgc.ErrRequestEntityTooLarge == nil {
+		bsgc.ErrRequestEntityTooLarge = &air.Error{
+			Code:    413,
+			Message: "Request Entity Too Large",
+		}
+	}
+
+	return func(next air.Handler) air.Handler {
+		return func(req *air.Request, res *air.Response) error {
+			if req.ContentLength > bsgc.MaxBytes {
+				return bsgc.ErrRequestEntityTooLarge
+			}
+
+			return next(req, res)
+		}
+	}
+}
