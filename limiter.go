d405551ed019971711ed8eaa1ae77954c0764052
Aofei Sheng <aofei@aofeisheng.com>
2018-09-19 14:41:53 +0800

    refactor: adapt to changes of modules

--- limiter.go
+++ limiter.go
@@ -1,27 +1,29 @@
 package limiter
 
-import "github.com/aofei/air"
+import (
+	"errors"
+
+	"github.com/aofei/air"
+)
 
 // BodySizeGasConfig is a set of configurations for the `BodySizeGas()`.
 type BodySizeGasConfig struct {
-	MaxBytes                 int64
-	ErrRequestEntityTooLarge *air.Error
+	MaxBytes int64
+	Error413 error
 }
 
 // BodySizeGas returns an `air.Gas` that is used to limit ervery request's body
 // size based on the bsgc.
 func BodySizeGas(bsgc BodySizeGasConfig) air.Gas {
-	if bsgc.ErrRequestEntityTooLarge == nil {
-		bsgc.ErrRequestEntityTooLarge = &air.Error{
-			Code:    413,
-			Message: "Request Entity Too Large",
-		}
+	if bsgc.Error413 == nil {
+		bsgc.Error413 = errors.New("Request Entity Too Large")
 	}
 
 	return func(next air.Handler) air.Handler {
 		return func(req *air.Request, res *air.Response) error {
 			if req.ContentLength > bsgc.MaxBytes {
-				return bsgc.ErrRequestEntityTooLarge
+				res.Status = 413
+				return bsgc.Error413
 			}
 
 			return next(req, res)
