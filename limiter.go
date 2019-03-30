0f38eb80c2bb302c18a12583498a99d57dcb1436
Aofei Sheng <aofei@aofeisheng.com>
2019-03-30 16:05:31 +0800

    refactor: refine `BodySizeGas`

--- limiter.go
+++ limiter.go
@@ -14,7 +14,8 @@ type BodySizeGasConfig struct {
 }
 
 // BodySizeGas returns an `air.Gas` that is used to limit ervery request's body
-// size based on the bsgc.
+// size based on the bsgc. It prevents clients from accidentally or maliciously
+// sending a large request and wasting server resources.
 func BodySizeGas(bsgc BodySizeGasConfig) air.Gas {
 	if bsgc.ErrRequestEntityTooLarge == nil {
 		bsgc.ErrRequestEntityTooLarge = errors.New(
@@ -29,7 +30,51 @@ func BodySizeGas(bsgc BodySizeGasConfig) air.Gas {
 				return bsgc.ErrRequestEntityTooLarge
 			}
 
+			req.Body = &maxBytesBody{
+				bsgc: bsgc,
+				req:  req,
+				res:  res,
+			}
+
 			return next(req, res)
 		}
 	}
+}
+
+// maxBytesReader is similar to the `io.LimitReader` but is intended for
+// limiting the size of incoming request bodies.
+type maxBytesBody struct {
+	bsgc BodySizeGasConfig
+	req  *air.Request
+	res  *air.Response
+	cl   int64
+}
+
+// Read implements the `io.Reader`.
+func (mbb *maxBytesBody) Read(b []byte) (n int, err error) {
+	if rl := mbb.bsgc.MaxBytes - mbb.cl; rl > 0 {
+		if int64(len(b)) > rl {
+			b = b[:rl]
+		}
+
+		n, err = mbb.req.Body.Read(b)
+	} else {
+		return 0, mbb.bsgc.ErrRequestEntityTooLarge
+	}
+
+	mbb.cl += int64(n)
+	if err == nil && mbb.bsgc.MaxBytes-mbb.cl <= 0 {
+		if mbb.res.Written {
+			mbb.res.Status = http.StatusRequestEntityTooLarge
+		}
+
+		err = mbb.bsgc.ErrRequestEntityTooLarge
+	}
+
+	return
+}
+
+// Close implements the `io.Closer`.
+func (mbb *maxBytesBody) Close() error {
+	return nil
 }
